4fdcfb93f8a0619b23454f5c93b88ebefb8f1ea4
Fulviaca <80987024+Fulviaca@users.noreply.github.com>
2021-07-10 23:45:00 +0200

    date

--- src/script.js
+++ src/script.js
@@ -32,10 +32,17 @@ if (hours <= 11) {
   currentTime.innerHTML = `${hours}:${minutes} pm`;
 }
 //
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
